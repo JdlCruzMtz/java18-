477f12159bdf62f3b0c0ca8375ce7c6e25d1d44b
JdlCruzMtz <delacruzmartinezjavier096@gmail.com>
2024-12-10 23:06:11 -0600

    first commit

---
+++ package com.mycompany.java
@@ -0,0 +1,13 @@
+package com.mycompany.unidad4;
+import javax.swing.JOptionPane; 
+
+public class ProbelmaNataly {
+    public static void main (String [] belisarius){
+        byte compra; 
+        
+        
+        JOptionPane.showInputDialog("CUANTAS PLAYERAS NECESITAS");
+        
+        
+    }
+}
